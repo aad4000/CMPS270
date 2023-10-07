5dcc38137e670dbe790fe62572b3173589837c8d
adk05 <adk05@mail.aub.edu>
2023-10-07 17:16:55 +0300

    fixed checking methods

--- checking.c
+++ checking.c
@@ -6,27 +6,16 @@
 // returns 1 if player has at least one spell he can play (check tally)
 // returns 0 if the player is out of option (loses)
 // takes the string of the previous spell played
-// NOT checked on first turn of first player (when last is NULL, return 1)
+// NOT checked on first turn of first player 
 int canPlay(struct Spell * spellMap[], int tally[], char * last) {
-    // If it's the first turn of the first player, return 1 (assuming last is NULL)
-    if (last == NULL) {
-        return 1;
-    }
+
 
     // Get the index of the last letter in the tally array
-    int index = last[0] - 'a';
+    int index = last[strlen(last)-1] - 'a';
 
     // Check if there are spells starting with the last letter that haven't been used
-    if (tally[index] > 0) {
-        struct Spell *current = spellMap[index];
-        while (current != NULL) {
-            if (current->used == 0) {
-                // The player has at least one spell they can play
-                return 1;
-            }
-            current = current->next;
-        }
-    }
+    if (tally[index] > 0) 
+        return 1;
 
     // The player is out of options (loses)
     return 0;
@@ -41,34 +30,34 @@ int canPlay(struct Spell * spellMap[], int tally[], char * last) {
 //      return -2 if        condition (2) was not satisfied
 //      return -3 if        condition (3) was not satisfied
 // if spell is legal, set used to 1 (true), and update tally
-int isLegal(struct Spell * spellMap[], int tally[], char * last, char * played) {
+int isLegal(struct Spell * spellMap[], int tally[], char * last, char * played, int round) {
     // Check condition (1): the first character of played matches last
-    if (last != NULL && played[0] != last[strlen(last) - 1]) {
-        return -1;  // Player loses because condition (1) was not satisfied
-    }
+    if(round != 0)
+        if (last != NULL && played[0] != last[strlen(last) - 1]) {
+            return -1;  // Player loses because condition (1) was not satisfied
+        }
 
     // Get the index of the first letter of played in the tally array
     int index = played[0] - 'a';
 
-    // Check condition (2): played is within the available spells
-    if (tally[index] == 0) {
-        return -2;  // Player loses because condition (2) was not satisfied
-    }
-
     // Check condition (3): the spell was not used before
     struct Spell *current = spellMap[index];
     while (current != NULL) {
-        if (strcmp(current->name, played) == 0 && current->used == 0) {
-            // The spell is legal, set used to 1 (true), and update tally
-            current->used = 1;
-            tally[index]--;
-            return 1;
+        if (strcmp(current->name, played) == 0) {
+            if(current->used == 0){
+                // The spell is legal, set used to 1 (true), and update tally
+                current->used = 1;
+                tally[index]--;
+                return 1;
+            }
+            else     // Check condition (2): played is within the available spells (checked later)
+                return -3;
         }
         current = current->next;
     }
 
-    // Player loses because condition (3) was not satisfied
-    return -3;
+    // Player loses because condition (2) was not satisfied
+    return -2;
 }
 
 
--- driver.c
+++ driver.c
@@ -30,59 +30,59 @@ int main(){
     int coin = rand()%2; // 0 for player 1, and 1 for player 2
 
     int c = 0;
-    char * prev = NULL;
+    char * prev = (char *) malloc(18 * sizeof(char));
 
     while(1){
-    int turn = ((c + coin) % 2);
+        int turn = ((c + coin) % 2);
 
-    if(!canPlay(spellMap, tally, prev)){
-        //Player loses because he has no more possible moves.
-        char * winner, *loser;
-        if(turn == 0){
-            winner = player2;
-            loser = player1;
-        }
-        else{
-            winner = player1;
-            loser = player2;
-        }
-        printf("\n%s lost because there are no more possible moves.\n%s is the winner!", loser, winner);
-        break;
-    }
-
-    if(turn == 0)
-        printf("\n%s's turn. Enter a spell: ", player1);
-    else 
-        printf("\n%s's turn. Enter a spell: ", player2);
-    
-    char word [18]; //again, assuming longest word is of 18 characters
-    scanf("%s", word);
-
-    int t = isLegal(spellMap, tally, prev, word);
-    if(t < 0){
-        //Player loses for one of 3 reasons according to return value
-        char * winner, *loser;
-        if(turn == 0){
-            winner = player2;
-            loser = player1;
-        }
-        else{
-            winner = player1;
-            loser = player2;
+        if(!canPlay(spellMap, tally, prev) && c!=0){
+            //Player loses because he has no more possible moves.
+            char * winner, *loser;
+            if(turn == 0){
+                winner = player2;
+                loser = player1;
+            }
+            else{
+                winner = player1;
+                loser = player2;
+            }
+            printf("\n%s lost because there are no more possible moves.\n%s is the winner!", loser, winner);
+            break;
         }
 
-        if(t == -1)
-            printf("\n%s lost because the first letter does not match the last letter of the previous spell.\n%s is the winner!", loser, winner);
-        else if(t == -2)
-            printf("\n%s lost because the spell used is not on the list.\n%s is the winner!", loser, winner);
+        if(turn == 0)
+            printf("\n%s's turn. Enter a spell: ", player1);
         else 
-            printf("\n%s lost because the spelled used was previously used.\n%s is the winner!", loser, winner);
+            printf("\n%s's turn. Enter a spell: ", player2);
+        
+        char word [18]; //again, assuming longest word is of 18 characters
+        scanf("%s", word);
 
-        break;
-    }
+        int t = isLegal(spellMap, tally, prev, word, c);
+        if(t < 0){
+            //Player loses for one of 3 reasons according to return value
+            char * winner, *loser;
+            if(turn == 0){
+                winner = player2;
+                loser = player1;
+            }
+            else{
+                winner = player1;
+                loser = player2;
+            }
 
-    prev = word;
-    c++;
+            if(t == -1)
+                printf("\n%s lost because the first letter does not match the last letter of the previous spell.\n%s is the winner!", loser, winner);
+            else if(t == -2)
+                printf("\n%s lost because the spell used is not on the list.\n%s is the winner!", loser, winner);
+            else 
+                printf("\n%s lost because the spelled used was previously used.\n%s is the winner!", loser, winner);
+
+            break;
+        }
+
+        strcpy(prev, word);
+        c++;
     }
 
 
